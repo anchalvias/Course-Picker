c192636b5bf3ab577ea6f0f1d458a2820165a748
anchalvias <anchalvias@gmail.com>
2020-11-03 22:35:51 -0500

    changed theme and added home page

--- wp-config.php
+++ wp-config.php
@@ -20,7 +20,7 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'fiucoursepicker' );
+define( 'DB_NAME', 'example' );
 
 /** MySQL database username */
 define( 'DB_USER', 'root' );
